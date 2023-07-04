07b1b6a7b485b37fed674e7abd152a4beb7bdbf0
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2023-07-04 16:09:09 +0700

    update

--- App.py
+++ App.py
@@ -2,11 +2,11 @@ from pygame._sdl2.video import Window,Texture,Image,Renderer
 import pygame
 class App:
 
-    def __init__(self,WIN_SIZE = [1366,768],Title = "pygame") -> None:
+    def __init__(self,WIN_SIZE = [1366,768],Title = "pygame", refresh_rate = 60, ) -> None:
         self.window = Window(title=Title,size = WIN_SIZE)
         self.renderer = Renderer(self.window)
         self.renderer.draw_color = (0,0,0,255)
-        self.refresh_rate = 60
+        self.refresh_rate = refresh_rate
         self.refresh_time_cnt = 0
         self.clock = pygame.time.Clock()
         self.dt = 0.0
@@ -18,13 +18,14 @@ class App:
                 quit()
 
     def update(self):
+        '''
+        Update delta time and refresh time
+        '''
         self.dt = self.clock.tick() * 0.001
         self.refresh_time_cnt += self.dt
 
     def draw(self):
-        self.renderer.clear()
-
-        self.renderer.present()
+        pass
 
     def run(self):
         while True:
@@ -33,3 +34,27 @@ class App:
             if self.refresh_time_cnt >= 1/self.refresh_rate:
                 self.draw()
                 self.refresh_time_cnt -= 1/self.refresh_rate
+
+class MainMenu(App):
+
+    def __init__(self, WIN_SIZE=[1366, 768], Title="pygame") -> None:
+        super().__init__(WIN_SIZE, Title)
+
+    def update(self):
+        super().update()
+    
+    def draw(self):
+        super().draw()
+    
+class SceneManager:
+
+    def __init__(self) -> None:
+        self.scenes = []
+    
+    def push(self,scene : App):
+        self.scenes.append(scene)
+    
+    def run(self):
+        if len(self.scenes > 0):
+            self.scenes[-1].run()
+        
--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import sys,math,os
 from pygame._sdl2.video import Window,Texture,Image,Renderer
 from sprite_classes import SpriteUnit,SpriteHandler
 from App import *
-
-
 if __name__ == "__main__":
     app = App()
     app.run()
